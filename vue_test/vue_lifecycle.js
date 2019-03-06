243bfcd625da59c99548c0167f8605f44bc12e0b
신용주 <lukas.sin@kakaocorp.com>
2019-03-06 13:15:16 +0900

    Vue_lifecycle

---
+++ vue_test/vue_lifecycle.js
@@ -0,0 +1,23 @@
+// vue 에서는 this 컨텍스트가 호출하는 인스턴스에서 에로우 함수를 
+// 사용할 경우 에러 남 
+
+var vm_lf1 = new Vue({
+  el : '#lf1',
+  data : {
+      a : 1
+  },
+  created : function(){
+    console.log('a is '+ this.a+' created')
+  },
+  mounted : function(){
+    // 컴포넌트 랜더링 시
+    this.a = 3
+    console.log('a is '+ this.a+' mounted')
+  },
+  destroyed : function(){
+    
+    // destroyed는 서버측에서는 호출 안됨 
+    this.a = 4
+    console.log('destroyed', this.a)
+  }
+})
